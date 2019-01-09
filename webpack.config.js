add14e931485f70ea89487a1727e4c78a22dd3e1
TatsianaHal <tripwithtanya@gmail.com>
2019-01-09 10:18:15 +0300

    Update 'webpack.config.js' file

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,49 @@
 let path = require('path');
+// let ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 let conf = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './dist'), // путь к файлу
-        filename: 'main.js'
+        filename: 'main.js',
+        // относительная ссылка на файл main.js, которая будет подставляться
+        // из браузера (конкатенируется с filename):
+        publicPath: 'dist/',
+
     },
-    module: {  // описание правил, что и с каким расширением делать
+    devServer: {
+        //  для легко и быстро заметить ошибку при загрузке
+        // прямо на экране браузера- не нужно лезть в консоль:
+        overlay: true,
+    },
+    module: {
+        // описание правил, что и с каким расширением делать:
         rules: [
+            // $ символизирует конец строки:
             {
-                test: /\.js$/, // $ символизирует конец строки
-                // ...
+                test: /\.js$/,
+                loader: 'babel-loader',
+                // чтоб не прогонять через babel-loader:
+                exclude: '/node_modules',
             },
-            {
-                test: /\.css$/,
-                // ...
-            }
+            // аналогично и для css:
+            // {
+            //     test: /\.css$/,
+            //     use: ExtractTextPlugin.extract({
+            //            fallback: 'style-loader',
+            //            use: 'css-loader',
+            //     })
+            // },
         ]
     },
-    plugins: [
-        new ExtractTextPlugin('style.css'),
-    ]
+    // plugins: [
+    //     new ExtractTextPlugin('style.css'),
+    // ]
 
 };
 
-module.exports = conf;
+module.exports = (env, options) => {
+    let production = options.mode === 'production';
+    conf.devtool = production ? false : 'eval-sourcemap';
+    return conf;
+};
